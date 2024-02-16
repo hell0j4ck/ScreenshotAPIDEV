1c5b819983e13a7dfa578891b08054598f10cd02
jacklee.jabra@yahoo.com <evilc4tofficial@gmail.com>
2024-02-15 20:07:59 -0500

    Major update:

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ app.set('pdfs', __dirname + '/pdfs')
 
 app.listen(port = '3001', () => {
 
-    console.log(`Listening on port ${port}...`)
+    console.log(`Listening on port ${port}...\n`)
 })
+
+
 
 
 app.get('/', (req, res) => {
@@ -34,17 +36,49 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
 
     const {format,url} = req.body
+
+    // Added HTTPS checking
+
+    const isHttps = url.slice(0,8)
+
+    if (isHttps !== 'https://'){
+
+        httpsurl = 'https://'+url
+
+        console.log(`This is the submitted URL: ${url}\n`)
+
+        console.log(`This is your fixed URL: ${httpsurl}\n`)
+
+        if (format==='screenshot'){
+
+            res.redirect(`/screenshot?url=${httpsurl}`)
     
-    if (format==='screenshot'){
+            
+        }else{
+    
+            res.redirect(`/pdf?url=${httpsurl}`)
+    
+        }
 
-        res.redirect(`/screenshot?url=${url}`)
+            
 
-        
     }else{
 
-        res.redirect(`/pdf?url=${url}`)
+        if (format==='screenshot'){
+
+            res.redirect(`/screenshot?url=${url}`)
+    
+            
+        }else{
+    
+            res.redirect(`/pdf?url=${url}`)
+    
+        }
+
 
     }
+    
+    
  
  })
  
@@ -60,89 +94,109 @@ app.post('/', (req, res) => {
 
 app.get('/pdf', async (req, res) => {
 
+
+
+
     // USE req.query to post url as query string
     // USE req.body to post the url in the request body from form
-    console.log('RECEIVED SOMETHING!')
-    console.log(req.body)
+    console.log(`RECEIVED REQUEST FOR PDF FILE FROM IP: ${req.ip}\n`)
+
+    console.log('Here is the query object:')
     console.log(req.query)
+    console.log('\n')
     
+    console.log('Extracting URL from query object...\n')
     const { url } = req.query
-    console.log(url)
 
-    // Use stealth 
-    puppeteer.use(pluginStealth()) 
-
-    // Creates browser instance 
-    const browser = await puppeteer.launch({headless:"new"});
-
-    // Creates page instance
-    const page = await browser.newPage();
+    console.log('URL Extracted:')
+    console.log(url+'\n')
 
 
-    page.setDefaultNavigationTimeout(300000)
+    if (url){
 
-    // If Link is not valid, it will redirect to the form submission via the catch 
-    try {
+            // Use stealth 
+        puppeteer.use(pluginStealth()) 
 
-        // Goes to URL and establishes page
-        await page.goto(url, { waitUntil: 'networkidle0' });
+        // Creates browser instance 
+        const browser = await puppeteer.launch({headless:"new"});
 
-         
-
-        await page.emulateMediaType('screen');
-
-
-        // Generates a unique ID for the PDF 
-        const fileId = uuid()
-        const fileName = `result-${fileId}.pdf`
+        // Creates page instance
+        const page = await browser.newPage();
 
 
+        page.setDefaultNavigationTimeout(300000)
 
-        // Prints to PDF and saves under PATH
-        const pdf = await page.pdf({
-            path: `./pdfs/result-${fileId}.pdf`,
-            margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
-            printBackground: true,
-            format: 'A4',
-        });
+        // If Link is not valid, it will redirect to the form submission via the catch 
+        try {
 
-      
-        
-        // Closes the browser session
+            // Goes to URL and establishes page
+            await page.goto(url, { waitUntil: 'networkidle0' });
 
-        await browser.close();
+            
 
-        // res.send("HELLO!")
-        res.sendFile(path.join(__dirname, `/pdfs/${fileName}`));
+            await page.emulateMediaType('screen');
 
 
-        // res.render('result.ejs', { fileId, fileName })   
-        
+            // Generates a unique ID for the PDF 
+            const fileId = uuid()
+            const fileName = `result-${fileId}.pdf`
+
+
+
+            // Prints to PDF and saves under PATH
+            const pdf = await page.pdf({
+                path: `./pdfs/result-${fileId}.pdf`,
+                margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+                printBackground: true,
+                format: 'A4',
+            });
 
         
-        // This uses the Filesystem module to delete the generated file after 10 seconds AFTER sending to the client
-        // This prevents the server from being overloaded with thousands of unwanted files
-        setTimeout(()=>{
+            
+            // Closes the browser session
 
-            fs.unlink(path.join(__dirname, `/pdfs/${fileName}`), function (err) {
-                if (err) throw err;
-                console.log(`File deleted!`);
-              });
+            await browser.close();
 
-        },10000)
+            // res.send("HELLO!")
+            res.sendFile(path.join(__dirname, `/pdfs/${fileName}`));
 
 
-    } catch (e) {
+            // res.render('result.ejs', { fileId, fileName })   
+            
 
-        
-        console.log('There Seems To Be An Error: ' + e)
-        res.sendFile(path.join(__dirname, `/pdfs/default.pdf`));
+            
+            // This uses the Filesystem module to delete the generated file after 10 seconds AFTER sending to the client
+            // This prevents the server from being overloaded with thousands of unwanted files
+            setTimeout(()=>{
+
+                fs.unlink(path.join(__dirname, `/pdfs/${fileName}`), function (err) {
+                    if (err) throw err;
+                    console.log(`File deleted!`);
+                });
+
+            },10000)
+
+
+        } catch (e) {
+
+            
+            console.log('There Seems To Be An Error: ' + e)
+            res.sendFile(path.join(__dirname, `/pdfs/default.pdf`));
 
 
 
 
+        }
+
+
+
+
+    }else{
+
+        res.send("Woah there. Whatchu tryna do wise guy? <a href='/'>Go Back</a>")
     }
 
+    
 
 
 
@@ -151,96 +205,118 @@ app.get('/pdf', async (req, res) => {
 
 app.get('/screenshot', async (req, res) => {
 
+    
+   
+    
     // USE req.query to post url as query string
     // USE req.body to post the url in the request body from form
-    console.log('RECEIVED SOMETHING!')
-    console.log(req.body)
+    console.log(`RECEIVED REQUEST FOR IMAGE FILE FROM IP: ${req.ip}\n`)
+
+    console.log('Here is the query object:')
     console.log(req.query)
+    console.log('\n')
     
+    console.log('Extracting URL from query object...\n')
     const { url } = req.query
-    console.log(url)
 
-    // Use stealth 
-    puppeteer.use(pluginStealth()) 
-    
-    // Creates browser instance 
-    const browser = await puppeteer.launch({headless:"new"});
-
-    // Creates page instance
-    const page = await browser.newPage();
-
-    page.setDefaultNavigationTimeout(300000)
-
-    // If Link is not valid, it will redirect to the form submission via the catch 
-    try {
-
-        // Goes to URL and establishes page
-        await page.goto(url, { waitUntil: 'networkidle0' });
-
-
-        await page.emulateMediaType('screen');
-
-
-        // Generates a unique ID for the PDF 
-        const fileId = uuid()
-        const fileName = `result-${fileId}.png`
+    console.log('URL Extracted:')
+    console.log(url+'\n')
 
 
 
-
-        // Takes a Screenshot and saves under PATH
-        await page.screenshot({
-            "type": "png", // can also be "jpeg" or "webp" (recommended)
-            "path": `./screenshots/result-${fileId}.png`,  // where to save it
-            "fullPage": true,  // will scroll down to capture everything if true
-        });
+    if(url){
 
 
         
-        // Closes the browser session
 
-        await browser.close();
+        // Use stealth 
+        puppeteer.use(pluginStealth()) 
+        
+        // Creates browser instance 
+        const browser = await puppeteer.launch({headless:"new"});
 
-      
-        res.sendFile(path.join(__dirname, `/screenshots/${fileName}`));
+        // Creates page instance
+        const page = await browser.newPage();
+
+        page.setDefaultNavigationTimeout(300000)
+
+        // If Link is not valid, it will redirect to the form submission via the catch 
+        try {
+
+            // Goes to URL and establishes page
+            await page.goto(url, { waitUntil: 'networkidle0' });
 
 
-        // This uses the Filesystem module to delete the generated file after 10 seconds AFTER sending to the client
-        // This prevents the server from being overloaded with thousands of unwanted files
-        setTimeout(()=>{
+            await page.emulateMediaType('screen');
 
-            fs.unlink(path.join(__dirname, `/screenshots/${fileName}`), function (err) {
-                if (err) throw err;
-                console.log(`File deleted!`);
-              });
 
-        },10000)
+            // Generates a unique ID for the PDF 
+            const fileId = uuid()
+            const fileName = `result-${fileId}.png`
+
+
+
+
+            // Takes a Screenshot and saves under PATH
+            await page.screenshot({
+                "type": "png", // can also be "jpeg" or "webp" (recommended)
+                "path": `./screenshots/result-${fileId}.png`,  // where to save it
+                "fullPage": true,  // will scroll down to capture everything if true
+            });
+
+
+            
+            // Closes the browser session
+
+            await browser.close();
 
         
+            res.sendFile(path.join(__dirname, `/screenshots/${fileName}`));
 
 
-    }catch(e) {
+            // This uses the Filesystem module to delete the generated file after 10 seconds AFTER sending to the client
+            // This prevents the server from being overloaded with thousands of unwanted files
+            setTimeout(()=>{
 
-        console.log('There Seems To Be An Error: ' + e)
-        res.sendFile(path.join(__dirname, `/pdfs/default.pdf`));
+                fs.unlink(path.join(__dirname, `/screenshots/${fileName}`), function (err) {
+                    if (err) throw err;
+                    console.log(`File deleted!`);
+                });
+
+            },10000)
+
+            
+
+
+        }catch(e) {
+
+            console.log('There Seems To Be An Error: ' + e)
+            res.sendFile(path.join(__dirname, `/pdfs/default.pdf`));
 
 
 
 
+        }
+
+
+
+    }else{
+
+        res.send("Woah there. Whatchu tryna do wise guy? <a href='/'>Go Back</a>")
     }
+
+    
 
 
 
 
 })
 
-// NOT REQUIRED FOR API
-app.get('/fetchPdf', (req, res) => {
 
-    const { fileName } = req.query
-    console.log(req.query)
-    res.sendFile(path.join(__dirname, `/pdfs/${fileName}`));
+app.get('*',(req,res)=>{
 
+    res.send("Woah there. Whatchu tryna do wise guy? <a href='/'>Go Back</a>")
+    
 })
 
 
